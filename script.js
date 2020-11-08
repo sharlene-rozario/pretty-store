917e525fbfd5d510110ca81bdc14e784629cd851
sharlenerozario <45494912+sharlenerozario@users.noreply.github.com>
2020-11-09 02:24:17 +0600

    2
    2

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//--------------------------slide---------------------
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+
+
+
+
+
+
+
+// -------------------------------nav bar in mobile--------------------------
 var MenuItems=document.getElementById("MenuItems");
 MenuItems.style.maxHeight="50px";
 function menutoggle(){
@@ -7,4 +31,21 @@ function menutoggle(){
     else{
         MenuItems.style.maxHeight="50px";
     }
+}
+
+// ---------------product gallery-----------------
+var ProductImg=document.getElementById("ProductImg");
+var SmallImg=document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick=function(){
+    ProductImg.src=SmallImg[0].src;
+}
+SmallImg[1].onclick=function(){
+    ProductImg.src=SmallImg[1].src;
+}
+SmallImg[2].onclick=function(){
+    ProductImg.src=SmallImg[2].src;
+}
+SmallImg[3].onclick=function(){
+    ProductImg.src=SmallImg[3].src;
 }
